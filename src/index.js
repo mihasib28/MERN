ab8d705e134cf5c94faa35fbe8387f944dbac362
mihasib <mihasib123@gmail.com>
2026-01-05 19:53:33 +0600

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,53 +1,18 @@
-import dotenv from "dotenv";
-import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
 
-const port = process.env.PORT || 3000;
 
-
-dotenv.config();
 
 connectDB()
-.then(()=>{
-  app.listen(port, () => {
-  console.log(`server listening on ${port}`)
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
 })
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
-.catch((err)=> {
-  console.log(`Database connection failed !!`, err);
-  
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_MERN}`);
-//       mongoose.connection.on("error", (err) => {
-//       console.log("MongoDB connection error:", err);
-//     });
-//      console.log("MongoDB connected");
-
-//     app.listen(PORT, () => {
-//       console.log(`Server running on http://localhost:${PORT}`);
-//     });
-//   } catch (error) {
-//     console.log("ERROR:", error);
-//     throw error;
-//   }
-// })();
